094aab80083bd9d6d68dbebee5e1e2fbcc44cc43
Joseph Downs <jos.w.downs@gmail.com>
2022-03-04 15:31:54 -0700

    Add feat read

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,13 +1,21 @@
-handle = File.open(ARGV[0], "r")
+require './writer'
 
-incoming_text = handle.read
+night_writer = Writer.new
+night_writer.run
 
-handle.close
 
-capitalized_text = incoming_text.upcase
 
-writer = File.open(ARGV[1], "w")
 
-writer.write(capitalized_text)
-
-writer.close
+# handle = File.open(ARGV[0], "r")
+# open_file_one
+# incoming_text = handle.read
+# read(file)
+# handle.close
+# close(file)
+# capitalized_text = incoming_text.upcase
+#
+# writer = File.open(ARGV[1], "w")
+#
+# writer.write(capitalized_text)
+#
+# writer.close
---
+++ lib/writer.rb
@@ -0,0 +1,43 @@
+class Writer
+
+  def run
+    file = open_file_one
+    read(file)
+    close(file)
+    file2 = open_file_two
+    write_file(file2)
+    close(file2)
+    print_statement
+  end
+
+  def open_file_one
+    File.open(ARGV[0], "r")
+  end
+
+  def read(file)
+    file.read
+  end
+
+  def close(file)
+    file.close
+  end
+
+  def open_file_two
+    File.open(ARGV[1], "w")
+  end
+
+  def write_file(file)
+    file.write
+  end
+
+  def characters
+    File.read(ARGV[0]).length
+  end
+
+  def print_statement
+    puts "Created #{ARGV[1]} conataining #{characters} characters"
+  end
+
+  def incoming_text
+
+end
