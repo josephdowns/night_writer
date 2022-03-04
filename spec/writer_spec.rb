860d27469373ae85e4c645ce44c7dc3830fc42d4
Joseph Downs <jos.w.downs@gmail.com>
2022-03-04 15:39:00 -0700

    Add test: Night writer class

--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -3,5 +3,13 @@ require 'rspec'
 require 'pry'
 
 describe Writer do
-  before (:each)
+  before (:each) do
+    ARGV[0] = './spec/test_folder/test_mes.txt'
+    ARGV[1] = './spec/test_folder/new_test_mes.txt'
+    @night_writer = Writer.new
+  end
+
+  it "is a class" do
+    expect(@night_writer).to be_a(Writer)
+  end
 end
