2d3260ae7624b7c798aff027179d70e3af8150f0
Joseph Downs <jos.w.downs@gmail.com>
2022-03-04 14:45:52 -0700

    Beginnings of new project

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,1 +1,13 @@
 handle = File.open(ARGV[0], "r")
+
+incoming_text = handle.read
+
+handle.close
+
+capitalized_text = incoming_text.upcase
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(capitalized_text)
+
+writer.close
