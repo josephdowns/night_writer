4d47b413ef534ddf9004419b65f185b3da64933e
Joseph Downs <jos.w.downs@gmail.com>
2022-03-07 11:21:28 -0700

    Add feat: translate able to handle multiple characters

--- lib/english_dictionary.rb
+++ lib/english_dictionary.rb
@@ -3,41 +3,48 @@ class EnglishDictionary
   attr_reader :english_dictionary
 
   def initialize
-    @english_dictionary = {["O", ".", ".", ".", ".", "."] => "a",
-                           ["O", ".", "O", ".", ".", "."] => "b",
-                           ["O", "O", ".", ".", ".", "."] => "c"}
+    @english_dictionary = {"O....." => "a", "O.O..." => "b", "OO...." => "c",
+                           "OO.O.." => "d", "O..O.." => "e", "OOO..." => "f",
+                           "OOOO.." => "g", "O.OO.." => "h", ".OO..." => "i",
+                           ".OOO.." => "j", "O...O." => "k", "O.O.O." => "l",
+                           "OO..O." => "m", "OO.OO." => "n", "O..OO." => "o",
+                           "OOO.O." => "p", "OOOOO." => "q", "O.OOO." => "r",
+                           ".OO.O." => "s", ".OOOO." => "t", "O...OO" => "u",
+                           "O.O.OO" => "v", ".OOO.O" => "w"
+
+
+                          }
   end
 
   def put_english(text)
-    word_count = text.strip.length
-    word_count_counter = (word_count / 4).ceil
-    array = []
-    first_array = []
-    second_array = []
-    array_count = 0
+    x = text.split("\n")
+    characters = (x[0].length / 2)
+    letters = []
+    letters_index = 0
+    index = 0
+    a = 0
+    message = []
 
-    word_count.times do
-      array << text[array_count]
-      array_count += 1
+    characters.times do
+      letters << []
     end
 
-    array.delete("\n")
-
-    if word_count_counter > 2
-      word_count_counter.times do
-        2.times do
-          first_array << array.shift
-        end
-
-        2.times do
-          second_array << array.shift
-        end
+    characters.times do
+      x.each do |line|
+        letters[index] << line[a..a+1]
       end
+      index += 1
+      a += 2
     end
 
-    # binding.pry
+    characters.times do
+      message << @english_dictionary[letters[letters_index].join]
+      letters_index += 1
+    end
 
-    english = "#{@english_dictionary[array]}" + "#{@english_dictionary[first_array.compact]}" + "#{@english_dictionary[second_array.compact]}"
+    return message.join
+
+
   end
 
 end
