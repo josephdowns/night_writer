b1e79905115c1cd9e36b50d55700c77ba69d4c87
Joseph Downs <jos.w.downs@gmail.com>
2022-03-06 22:30:51 -0700

    Add test: translate two characters

--- lib/english_dictionary.rb
+++ lib/english_dictionary.rb
@@ -6,4 +6,20 @@ class EnglishDictionary
     @english_dictionary = {["O", ".", ".", ".", ".", "."] => "a"}
   end
 
+  def put_english(text)
+    word_count = text.strip.length
+    array = []
+    array_count = 0
+
+    word_count.times do
+      array << text[array_count]
+      array_count += 1
+    end
+
+    array.delete("\n")
+    # binding.pry
+
+    english = @english_dictionary[array]
+  end
+
 end
--- spec/english_dictionary_spec.rb
+++ spec/english_dictionary_spec.rb
@@ -10,5 +10,15 @@ describe EnglishDictionary do
     it "is a class" do
       expect(@dictionary).to be_a(EnglishDictionary)
     end
+
+    it "puts to alpha characters" do
+      text = "O.\n..\n.."
+      expect(@dictionary.put_english(text)).to eq("a")
+    end
+
+    it "gets a little more complicated" do
+      text = "O.O.\n..O.\n...."
+      expect(@dictionary.put_english(text)).to eq("ab")
+    end
   end
 end
