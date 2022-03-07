8c35ff4ca0af4bd32b96973c6f10be552fdd0457
Joseph Downs <jos.w.downs@gmail.com>
2022-03-06 20:54:09 -0700

    Add test open file1

--- lib/reader.rb
+++ lib/reader.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 class Reader
+
+  def initialize
+    @incoming_text = incoming_text
+    @english_text = nil
+  end
+
+  def run
+    file = open_file_one
+    read = read(file)
+    file2 = open_file_two
+    english_text
+    write_file(file2)
+    print_statement
+  end
+
   
 end
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -6,10 +6,21 @@ describe Reader do
   context "first steps" do
     before (:each) do
       @reader = Reader.new
+      ARGV[0] = '.spec/test_folder/test_braille.txt'
+      ARGV[1] = '.spec/test_folder/new_eng_mess.txt'
+      @file1 = @reader.open_file_one
+      @file2 = @reader.open_file_one
     end
 
     it "is a class" do
       expect(@reader).to be_a(Reader)
     end
+
+    it "opens a file" do
+      expect(@file1).to be_a(File)
+    end
+
+    it "opens a second file" do
+      
   end
 end
